a136302cbb3f0bf7e987f524acdd0cd451a09de5
apple310565 <apple310565@gmail.com>
2022-01-24 08:08:34 +0800

    Bus Page complete 1. 原先只能讀到未出發的車，目前更正為都能讀到，未出發出的顯示預計到達時間(HH:MM)，已出發車輛顯示還有幾分鐘到達，在兩分鐘以內的顯示為即將到達，並且針對了末班車做了額外處理。 2. 修正了文涵提到的問題(第一次點會沒更新，回去再點就有了)，目前已完成，可以正常使用
    To-do:
    校園內的兩站u-bike的即時查詢

--- app/src/main/java/flag/com/ncubus/ui/busstops/BusstopsFragment.java
+++ app/src/main/java/flag/com/ncubus/ui/busstops/BusstopsFragment.java
@@ -8,8 +8,10 @@ import androidx.fragment.app.FragmentResultListener;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -39,6 +41,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -75,6 +78,7 @@ public class BusstopsFragment extends Fragment {
                 String busNumber = bundle.getString("busNumber");
                 Integer direction = bundle.getInt("direction");
                 //取得班次和方向，開始查所有站名+抵達時間
+
                 String url = "https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taoyuan/"+busNumber
                         +"?%24select=StopName%20%2C%20NextBusTime&%24filter=Direction%20eq%20"+direction.toString()+"&%24format=JSON";
                 bus(url);
@@ -82,6 +86,10 @@ public class BusstopsFragment extends Fragment {
                 // TODO: 只要能把站名和抵達時間，存到上面的 String[]就可以了
                 //  有存到，但是有神奇的bug: 點第一次沒有蓋到，但在繼續點就都有......
                 //  然後會有點當掉 -> 有時候沒有更新，比方說不管點哪個都是2分鐘前的某一班的清單
+                TextView textview = (TextView)getView().findViewById(R.id.busNumber);
+                textview.setText(busNumber);
+                // TODO: 上面提到的問題修正了，會有那個問題應該是因為thread是獨立執行的，所以主程式不會等thread將資料改完才回傳，處理方式是把底下的東西搬到下面
+                /*
                 if (NextBusTimes.size()!=0) {
                     //用 StopSequences 排序的序號排序
                     List<String> tmp_stops = new ArrayList<String>();
@@ -97,9 +105,7 @@ public class BusstopsFragment extends Fragment {
                 updateBuslist(); //更新站名+抵達時間
                 TextView Update_time =(TextView)getView().findViewById(R.id.Update_time);
                 Update_time.setText("最後更新時間: " + global_SrcUpdateTime);
-
-                TextView textview = (TextView)getView().findViewById(R.id.busNumber);
-                textview.setText(busNumber);
+                */
 
             }
         });
@@ -149,7 +155,10 @@ public class BusstopsFragment extends Fragment {
     }
 
     public void bus(String urlString){
+        ProgressDialog dialog = ProgressDialog.show(getContext(),"讀取中"
+                ,"請稍候",true);
         new Thread(() -> { //一次性Thread
+            Log.e("[Connect]",urlString);
             //String urlString = "https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taoyuan/132?%24select=StopName%20%2C%20NextBusTime&%24filter=Direction%20eq%201&%24format=JSON\n";
             try {
                 // 初始化 URL
@@ -187,14 +196,36 @@ public class BusstopsFragment extends Fragment {
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
                         String Zh_tw = jsonObject.getJSONObject("StopName").getString("Zh_tw");
-                        String NextBusTime = jsonObject.getString("NextBusTime");
                         String SrcUpdateTime = jsonObject.getString("SrcUpdateTime");
                         int StopSequence = jsonObject.getInt("StopSequence");
-                        NextBusTime = NextBusTime.substring(11, 16);
                         SrcUpdateTime = SrcUpdateTime.substring(11, 16);
-
+                        String NextBusTime="";
+                        if(!jsonObject.isNull("NextBusTime")){
+                            NextBusTime = jsonObject.getString("NextBusTime");
+                            NextBusTime=NextBusTime.substring(11, 16);
+                        }
+                        else{
+                            NextBusTime="末班車已駛離";
+                        }
+                        if(!jsonObject.isNull("EstimateTime")){
+                            Log.d("[LOG Estimates] ", jsonObject.getString("EstimateTime"));
+                            Calendar calendar = Calendar.getInstance();
+                            int hour=calendar.get(Calendar.HOUR_OF_DAY)+8;
+                            if(hour>24)hour-=24;
+                            int minute=calendar.get(Calendar.MINUTE);
+                            int second=calendar.get(Calendar.SECOND);
+                            Log.d("[Time]",String.valueOf(hour)+":"+String.valueOf(minute)+":"+String.valueOf(second));
+                            String [] tmp_calendar=(jsonObject.getString("SrcUpdateTime").substring(11,19)).split(":");
+                            double s = (Double.valueOf(tmp_calendar[0]))*3600+Double.valueOf(tmp_calendar[1])*60+Double.valueOf(tmp_calendar[2]);
+                            double s2 = Double.valueOf(hour*3600+minute*60+second);
+                            int num=(int)((jsonObject.getDouble("EstimateTime")-s2+s)/60);
+                            if(num<=1)NextBusTime="即將到站";
+                            else NextBusTime=String.valueOf(num)+"分鐘";
+                        }
+                        else{
+                            Log.d("[LOG Estimates] ", "None");
+                        }
                         Log.e("[LOG] ", Zh_tw + ": " + NextBusTime);
-
                         //add items
                         global_SrcUpdateTime.set(SrcUpdateTime);
                         Zh_tws.add(Zh_tw);
@@ -202,6 +233,41 @@ public class BusstopsFragment extends Fragment {
                         StopSequences.add(StopSequence);
                     }
                 }
+                //Child Thread和Parents對接的通道
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        dialog.dismiss();
+                        TextView Update_time =(TextView)getView().findViewById(R.id.Update_time);
+                        if (NextBusTimes.size()!=0) {
+                            //用 StopSequences 排序的序號排序
+                            List<String> tmp_stops = new ArrayList<String>();
+                            List<String> tmp_times = new ArrayList<String>();
+                            for( int i=1; i<=StopSequences.size(); i++){
+                                int index = StopSequences.indexOf(i);
+                                tmp_stops.add(Zh_tws.get(index));
+                                tmp_times.add(NextBusTimes.get(index));
+                            }
+                            ArriveTimes = tmp_times.toArray(new String[0]);
+                            BusStops = tmp_stops.toArray(new String[0]);
+                            updateBuslist(); //更新站名+抵達時間
+                            Update_time.setText("最後更新時間: " + global_SrcUpdateTime);
+                        }
+                        else{
+                            updateBuslist(); //更新站名+抵達時間
+                            Update_time.setText("最後更新時間: None");
+                            new AlertDialog.Builder(getActivity())
+                                    .setTitle("提醒")
+                                    .setMessage("此班公車的末班車已過!")
+                                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                        }
+                                    })
+                                    .show();
+                        }
+                    }
+                });
             }
             catch (IOException | JSONException e) {
                 e.printStackTrace();
